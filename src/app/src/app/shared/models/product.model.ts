99afd286d48c822af218be39ba6cdfd88919e221
Wale <onafesowale@gmail.com>
2023-11-21 06:00:00 +0100

    product model

---
+++ src/app/src/app/shared/models/product.model.ts
@@ -0,0 +1,10 @@
+export default interface IProduct{
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  yearOfRelease: number;
+  imageUrl: number;
+  category: string;
+  brand: string;
+}
