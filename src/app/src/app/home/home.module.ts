795a5dc1dd93aa906f41a072086c40856c3977dc
Wale <onafesowale@gmail.com>
2023-11-22 10:38:40 +0100

    home module

--- src/app/src/app/home/home.module.ts
+++ src/app/src/app/home/home.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { FilterItemComponent } from './filter-item/filter-item.component';
+import { FooterComponent } from './footer/footer.component';
+import { FilterComponent } from './filter/filter.component';
+import { FormsModule } from '@angular/forms';
+import { SliderModule } from 'primeng/slider';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    FilterItemComponent,
+    FooterComponent,
+    FilterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    SliderModule
   ]
 })
 export class HomeModule { }
