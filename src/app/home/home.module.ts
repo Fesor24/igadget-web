b822706f06791d1a62dec786f59a5c9e12a6bfe7
Wale <onafesowale@gmail.com>
2023-11-23 04:31:56 +0100

    added paginator module

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { FilterItemComponent } from './filter-item/filter-item.component';
-import { FooterComponent } from './footer/footer.component';
-import { FilterComponent } from './filter/filter.component';
 import { FormsModule } from '@angular/forms';
 import { SliderModule } from 'primeng/slider';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { ProductsComponent } from './products/products.component';
+import { FilterComponent } from './filter/filter.component';
+import { PaginatorModule } from 'primeng/paginator';
 
 
 
@@ -13,13 +15,15 @@ import { SliderModule } from 'primeng/slider';
   declarations: [
     HomeComponent,
     FilterItemComponent,
-    FooterComponent,
-    FilterComponent
+    FilterComponent,
+    ProductItemComponent,
+    ProductsComponent
   ],
   imports: [
     CommonModule,
     FormsModule,
-    SliderModule
+    SliderModule,
+    PaginatorModule
   ]
 })
 export class HomeModule { }
