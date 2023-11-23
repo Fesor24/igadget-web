b5b866b06e876f77ff4d292d872a9ac4fc105ed3
Wale <onafesowale@gmail.com>
2023-11-23 17:47:25 +0100

    shop component

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
   styleUrls: ['./shop.component.scss']
 })
-export class ShopComponent {
+export class ShopComponent implements OnInit {
+
+productId: string = '';
+
+  ngOnInit(): void {
+    this.getProduct();
+  }
+
+  constructor(private activatedRoute: ActivatedRoute){}
+
+  getProduct(){
+    this.activatedRoute.params.subscribe({
+      next: (params: Params) => this.productId = params['id'],
+      error: (err) => console.log(err)
+    })
+  }
 
 }
