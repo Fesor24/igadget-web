0be312b02ee183ceb0e7d7354041cd95a361eca7
Wale <onafesowale@gmail.com>
2023-11-23 17:46:40 +0100

    paging functionality

--- src/app/home/products/products.component.ts
+++ src/app/home/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import IPagination from 'src/app/shared/models/pagination.model';
 import IProduct from 'src/app/shared/models/product.model';
 
@@ -7,11 +7,22 @@ import IProduct from 'src/app/shared/models/product.model';
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnChanges {
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes['products'] && !changes['products'].firstChange){
+      this.pagingItemEnd = (this.pageSize * this.pageNumber) > this.products.totalCount ?
+      this.products.totalCount : this.pageSize * this.pageNumber;
+      this.pagingItemStart = (this.pageSize * (this.pageNumber - 1)) + 1;
+    }
+  }
 
   @Input() products!: IPagination<IProduct[]>;
 
   @Output() pageNumberEmitter = new EventEmitter<number>();
+
+  pagingItemStart = 1;
+
+  pagingItemEnd = 6;
 
   pageSize = 6;
 
