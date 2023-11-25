3c75815a3aedc51340994e930c90b646ee6130b9
Wale <onafesowale@gmail.com>
2023-11-25 23:04:08 +0100

    cart component

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CartService } from './cart.service';
+import { Observable } from 'rxjs';
+import { IShoppingCart, IShoppingCartTotals } from '../shared/models/cart.model';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss']
 })
-export class CartComponent {
 
+
+export class CartComponent implements OnInit {
+
+  cart$!: Observable<IShoppingCart | null>;
+  cartTotal$!: Observable<IShoppingCartTotals>;
 
   constructor(private cartService: CartService){}
+
+
+  ngOnInit(): void {
+    this.getCart();
+    this.getCartTotals();
+  }
+
+  getCart(){
+    this.cart$ = this.cartService.cart$;
+  }
+
+  getCartTotals(){
+    this.cartTotal$ = this.cartService.cartTotal$
+  }
+
+  // incrementCartItem(item){
+  //   this.cartService.addItemToCart()
+  // }
 
 }
