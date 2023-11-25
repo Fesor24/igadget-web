8bcfe220fcfdb6d102786ede78b5c3a364e032f6
Wale <onafesowale@gmail.com>
2023-11-25 18:03:37 +0100

    nav bar component

--- src/app/core/nav-bar/nav-bar.component.ts
+++ src/app/core/nav-bar/nav-bar.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/cart/cart.service';
+import { IShoppingCart, ShoppingCart } from 'src/app/shared/models/cart.model';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
+
+  cart: IShoppingCart = new ShoppingCart();
+
+  constructor(private cartService: CartService){}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  getCart(){
+    const cart = this.cartService.getCurrentCartValue();
+
+    if(cart){
+      this.cart = cart;
+    }
+
+
+  }
 
 }
