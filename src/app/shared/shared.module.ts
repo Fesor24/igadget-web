232b31d8484529b51cf8c8182d94d27dfc990068
Wale <onafesowale@gmail.com>
2023-11-25 23:06:26 +0100

    Order summary component export and declaration

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { OrderSummaryComponent } from './components/order-summary/order-summary.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    OrderSummaryComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    OrderSummaryComponent
   ]
 })
 export class SharedModule { }
