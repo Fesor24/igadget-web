da2723799a9bc35682b90f5381107254b6b77352
Wale <onafesowale@gmail.com>
2023-11-25 23:05:47 +0100

    order summary component

---
+++ src/app/shared/components/order-summary/order-summary.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { IShoppingCartTotals } from '../../models/cart.model';
+
+@Component({
+  selector: 'app-order-summary',
+  templateUrl: './order-summary.component.html',
+  styleUrls: ['./order-summary.component.scss']
+})
+export class OrderSummaryComponent {
+@Input() cartTotals!: IShoppingCartTotals;
+}
