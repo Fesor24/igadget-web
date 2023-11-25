e30f842f46dfd9b5c33220b2e33ddb96542f021d
Wale <onafesowale@gmail.com>
2023-11-25 09:45:35 +0100

    cart model

---
+++ src/app/shared/models/cart.model.ts
@@ -0,0 +1,12 @@
+import { IShoppingCartItem } from "./cart-item.model";
+import {v4 as uuidv4} from 'uuid';
+
+export interface IShoppingCart{
+  id: string;
+  items: IShoppingCartItem[];
+}
+
+export class ShoppingCart implements IShoppingCart{
+  id: string = uuidv4();
+  items!: IShoppingCartItem[];
+}
