09983003575374ad1aa4d1bcfb3fb66c1e5c6eec
Wale <onafesowale@gmail.com>
2023-11-25 23:03:43 +0100

    get cart when app initializes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from './cart/cart.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'IGadget';
+
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.getCart();
+  }
+
+  getCart(){
+    const cartId = localStorage.getItem('cart_id');
+
+    if(cartId){
+      this.cartService.getShoppingCart(cartId).subscribe({
+        next: () => console.log('Cart Initialized'),
+        error: err => console.log(err)
+      });
+    }
+
+  }
 }
