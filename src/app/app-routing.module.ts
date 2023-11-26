5e21a51e0521aee4fc13636d203afca025beec41
Wale <onafesowale@gmail.com>
2023-11-26 06:21:24 +0100

    added checkout route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   {path: '', loadChildren: () => import('./home/home.module').then(x => x.HomeModule)},
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(x => x.ShopModule)},
   {path: 'cart', loadChildren: () => import('./cart/cart.module').then(x => x.CartModule)},
+  {path: 'checkout', loadChildren: () => import('./checkout/checkout.module').then(x => x.CheckoutModule)},
   {path: '**', redirectTo: ''}
 ];
 
