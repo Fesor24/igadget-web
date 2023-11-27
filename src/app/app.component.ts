06e811e195105ca77e67686c788563204a91f9a4
Wale <onafesowale@gmail.com>
2023-11-27 11:14:00 +0100

    app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from './cart/cart.service';
+import { LoginResponse, OidcSecurityService } from 'angular-auth-oidc-client';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +11,12 @@ import { CartService } from './cart/cart.service';
 export class AppComponent implements OnInit {
   title = 'IGadget';
 
-  constructor(private cartService: CartService) {}
+  constructor(private cartService: CartService, private oidcService: OidcSecurityService,
+    private httpClient: HttpClient) {}
 
   ngOnInit(): void {
     this.getCart();
+    this.checkAuthStatus();
   }
 
   getCart(){
@@ -25,5 +29,26 @@ export class AppComponent implements OnInit {
       });
     }
 
+  }
+
+  checkAuthStatus(){
+    this.oidcService.checkAuth().subscribe({
+      next: (response: LoginResponse) => {
+        if(response.isAuthenticated){
+          console.log(response);
+          let httpHeaders = new HttpHeaders();
+
+          httpHeaders = httpHeaders.set('Authorization', `Bearer ${response.accessToken}`)
+
+          this.httpClient.get('http://localhost:5000/connect/userinfo', {headers: httpHeaders})
+          .subscribe({
+            next: res => console.log(res),
+            error: err => console.log(err)
+          });
+
+        }
+      },
+      error: err => console.log(err)
+    })
   }
 }
