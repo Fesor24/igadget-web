c1e552b809702dbfed5941cd828c12551af142d9
Wale <onafesowale@gmail.com>
2023-11-27 11:15:01 +0100

    added props to environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  apiUrl: 'http://localhost:6001/'
+  apiUrl: 'http://localhost:6001/',
+  idsvc: 'http://localhost:5000',
+  client_id: 'web_client',
+  scope: 'openid profile orderapi.full',
+  client_secret: 'web-secret'
 };
