6a041c0c359956bb0e2c5a0f424cd165fa332f7b
Wale <onafesowale@gmail.com>
2023-11-28 11:47:21 +0100

    checkout component

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart/cart.service';
+import { IShoppingCartTotals } from '../shared/models/cart.model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.scss']
 })
-export class CheckoutComponent {
+export class CheckoutComponent implements OnInit {
+
+  cartTotal$!: Observable<IShoppingCartTotals>;
+
+  constructor(private cartService: CartService){}
+
+
+  ngOnInit(): void {
+    this.getCartTotals();
+  }
+
+
+  getCartTotals(){
+    this.cartTotal$ = this.cartService.cartTotal$;
+  }
 
 }
