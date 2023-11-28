52cb9b60ecf4932764410198f59bdc64ab3d9cf2
Wale <onafesowale@gmail.com>
2023-11-28 11:48:52 +0100

    shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,19 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrderSummaryComponent } from './components/order-summary/order-summary.component';
 import { RouterModule } from '@angular/router';
+import { CdkStepperModule } from '@angular/cdk/stepper';
+import { StepperComponent } from './components/stepper/stepper.component';
 
 
 
 @NgModule({
   declarations: [
-    OrderSummaryComponent
+    OrderSummaryComponent,
+    StepperComponent
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    CdkStepperModule
   ],
   exports: [
-    OrderSummaryComponent
+    OrderSummaryComponent,
+    CdkStepperModule,
+    StepperComponent
   ]
 })
 export class SharedModule { }
