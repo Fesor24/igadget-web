2a7235df27a8bc686a147280784d96239c9833f5
Wale <onafesowale@gmail.com>
2023-11-28 16:53:54 +0100

    get delivery methods from api

--- src/app/checkout/checkout.service.ts
+++ src/app/checkout/checkout.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
+import { IDeliveryMethods } from '../shared/models/delivery.model';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CheckoutService {
 
-  constructor() { }
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getDeliveryMethods(){
+    return this.http.get<IDeliveryMethods[]>(`${this.baseUrl}delivery`).pipe(
+      map((delivery) => {
+        return delivery.sort((a,b) => b.price - a.price);
+      })
+    )
+  }
 }
