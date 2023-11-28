aa9e00b1c45f446ca7626ebe037e704d10dc7b59
Wale <onafesowale@gmail.com>
2023-11-28 08:43:28 +0100

    added checkout routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
   {path: '', loadChildren: () => import('./home/home.module').then(x => x.HomeModule)},
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(x => x.ShopModule)},
   {path: 'cart', loadChildren: () => import('./cart/cart.module').then(x => x.CartModule)},
-  {path: 'checkout', loadChildren: () => import('./checkout/checkout.module').then(x => x.CheckoutModule)},
+  {path: 'checkout',
+  loadChildren: () => import('./checkout/checkout.module').then(x => x.CheckoutModule),
+  canActivate: [authGuard]
+},
   {path: '**', redirectTo: ''}
 ];
 
