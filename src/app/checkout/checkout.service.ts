77f0417ccda38939e33a0b88bf1ec5bdea15beb4
Wale <onafesowale@gmail.com>
2023-11-29 10:26:08 +0100

    create order

--- src/app/checkout/checkout.service.ts
+++ src/app/checkout/checkout.service.ts
@@ -1,8 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment.development';
 import { IDeliveryMethods } from '../shared/models/delivery.model';
-import { map } from 'rxjs';
+import { map, of, switchMap } from 'rxjs';
+import { IOrder, IOrderCreate } from '../shared/models/order.model';
+import { AccountService } from '../account/account.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +13,7 @@ export class CheckoutService {
 
   baseUrl = environment.apiUrl;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private accountService: AccountService) { }
 
   getDeliveryMethods(){
     return this.http.get<IDeliveryMethods[]>(`${this.baseUrl}delivery`).pipe(
@@ -19,5 +21,23 @@ export class CheckoutService {
         return delivery.sort((a,b) => b.price - a.price);
       })
     )
+  }
+
+  createOrder(order: IOrderCreate){
+    return this.accountService.user$.pipe(
+      switchMap(user => {
+        if(user.isAuthenticated){
+          let httpHeaders = new HttpHeaders();
+
+          httpHeaders = httpHeaders.set('Authorization', `Bearer ${user.accessToken}`);
+
+          return this.http.post<IOrder>(`${this.baseUrl}order`, order, {headers: httpHeaders});
+        }else{
+          this.accountService.login();
+          return of(null)
+        }
+      })
+    )
+
   }
 }
