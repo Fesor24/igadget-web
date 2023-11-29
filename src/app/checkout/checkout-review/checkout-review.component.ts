1a255b14022adad17048d3d7f915795a29e36267
Wale <onafesowale@gmail.com>
2023-11-29 10:24:22 +0100

    checkout review component

--- src/app/checkout/checkout-review/checkout-review.component.ts
+++ src/app/checkout/checkout-review/checkout-review.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CartService } from 'src/app/cart/cart.service';
+import { IShoppingCart } from 'src/app/shared/models/cart.model';
+import { CheckoutService } from '../checkout.service';
+import { ToastrService } from 'ngx-toastr';
+import { IOrderCreate } from 'src/app/shared/models/order.model';
+import { FormGroup } from '@angular/forms';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-checkout-review',
   templateUrl: './checkout-review.component.html',
   styleUrls: ['./checkout-review.component.scss']
 })
-export class CheckoutReviewComponent {
+export class CheckoutReviewComponent implements OnInit {
+  cart$!: Observable<IShoppingCart>;
+  @Input() checkoutForm!: FormGroup;
 
+  constructor(private cartService: CartService, private checkoutService: CheckoutService,
+    private toastr: ToastrService, private router: Router){}
+
+  ngOnInit(): void {
+    this.getCart()
+  }
+
+  getCart(){
+    this.cart$ = this.cartService.cart$;
+  }
+
+  onSubmitOrder(){
+    const cart = this.cartService.getCurrentCartValue();
+    const orderCreate = this.createOrder(cart);
+
+    this.checkoutService.createOrder(orderCreate).subscribe({
+      next: order => {
+        if(order){
+          this.toastr.success('Order created successfully', 'Order created');
+          this.cartService.deleteLocalCart();
+
+          const navigationExtras: NavigationExtras = {state: order}
+
+          this.router.navigate(['/checkout/success'], navigationExtras);
+        };
+      },
+      error: err => {
+        this.toastr.error(err);
+        console.log(err);
+      }
+    })
+  }
+
+  private createOrder(cart: IShoppingCart) :IOrderCreate {
+    return {
+      cartId: cart.id,
+      deliveryMethodId: this.checkoutForm.get("deliveryForm")?.get("deliveryMethod")?.value,
+      deliveryAddress: this.checkoutForm.get("addressForm")?.value
+    }
+  }
 }
