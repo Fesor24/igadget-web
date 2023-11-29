d0b7816c0db98c2ee6c1a65204214b1456794d35
Wale <onafesowale@gmail.com>
2023-11-29 16:02:48 +0100

    order routing

---
+++ src/app/orders/orders-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { OrdersComponent } from './orders.component';
+import { OrderComponent } from './order/order.component';
+
+const routes: Routes = [
+  {path: '', component: OrdersComponent},
+  {path: ':id', component: OrderComponent}
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class OrdersRoutingModule { }
