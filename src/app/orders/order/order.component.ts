07c955504d1a60bda880c8d9f20b62d5357424f4
Wale <onafesowale@gmail.com>
2023-11-30 14:38:04 +0100

    order component

--- src/app/orders/order/order.component.ts
+++ src/app/orders/order/order.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Data } from '@angular/router';
+import { IOrder } from 'src/app/shared/models/order.model';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.scss']
 })
-export class OrderComponent {
+export class OrderComponent implements OnInit {
 
+  order!: IOrder
+
+constructor(private activatedRoute: ActivatedRoute){}
+
+  ngOnInit(): void {
+    this.getOrder();
+  }
+
+  getOrder(){
+    this.activatedRoute.data.subscribe({
+      next: (data: Data) => this.order = data['order'],
+      error: err => console.log(err)
+    })
+  }
 }
